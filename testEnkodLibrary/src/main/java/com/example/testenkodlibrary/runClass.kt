4a077beb86b4e6506bf8a89e56c688242c502b80
Andrey Pogodin <andreyweather@gmail.com>
2023-09-26 17:28:08 +0300

    Initial commit

---
+++ testEnkodLibrary/src/main/java/com/example/testenkodlibrary/runClass.kt
@@ -0,0 +1,10 @@
+package com.example.testenkodlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class runClass {
+    fun go (context: Context) {
+        Toast.makeText(context, "go", Toast.LENGTH_SHORT).show()
+    }
+}
